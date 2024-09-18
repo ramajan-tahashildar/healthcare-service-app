6e26e0411bad3c892b3735b3c8df4862418b75e9
Ramajan Tahashildar <154545514+ramajan-tahashildar@users.noreply.github.com>
2024-09-18 20:19:47 +0530

    Add files via upload

---
+++ models/Service.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const serviceSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Service = mongoose.model('Service', serviceSchema);
+
+module.exports = Service;
